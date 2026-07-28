6640032eae78f5838feed5729944c5a25a9c2915
Rizky <rizky@MacBook-Air-Rizky.local>
2026-07-28 22:59:43 +0700

    Revert api.js for local testing

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { supabase } from './supabaseClient';
 
-const BASE_URL = "https://komawan-be.noeronlabs.cloud/api/v1";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000/api/v1";
 
 const api = axios.create({
   baseURL: BASE_URL,
